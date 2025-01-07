67bc522a2fff28312146fb1cd40d2c831b3ad446
Albez0-An7h <ansh.km7096@gmail.com>
2025-01-07 22:03:05 +0530

    Adding use of for-each loop.

---
+++ Arrays/Enhancedfor.java
@@ -0,0 +1,11 @@
+public class Enhancedfor {
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,7,8,5,9};
+
+        System.out.println("The square of each element is : ");
+
+        for(int i:arr){
+            System.out.print(i*i + " ");
+        }
+    }
+}
